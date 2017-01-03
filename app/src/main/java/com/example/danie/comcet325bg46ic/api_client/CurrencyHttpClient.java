059a80309469cc7a5e8974aa160f05304e51e369
Danielle Potts <danielle-19-93@live.com>
2017-01-03 21:48:17 +0000

    minor refactor. add new images

--- app/src/main/java/com/example/danie/comcet325bg46ic/api_client/CurrencyHttpClient.java
+++
@@ -1,8 +0,0 @@
-package com.example.danie.comcet325bg46ic.api_client;
-
-/**
- * Created by danie on 23/12/2016.
- */
-public class CurrencyHttpClient{
-    //TODO: add api requests
-}
---
+++ app/src/main/java/com/example/danie/comcet325bg46ic/helpers/PopulateDatabase.java
@@ -0,0 +1,7 @@
+package com.example.danie.comcet325bg46ic.helpers;
+
+/**
+ * Created by danie on 03/01/2017.
+ */
+public class PopulateDatabase {
+}
